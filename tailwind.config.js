7ea12938a969c6da003fc49d542228af208e8836
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-05-20 22:46:11 +0400

    first

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif']
+      },
+
+      colors: {
+        'gega-red': '#bc1a45',
+        'gega-melon': '#ffd369',
+        'gega-grey': '#dddddd',
+        'gega-white': '#f7f7f7',
+      },
+    },
+  },
+  plugins: [],
+}
+
